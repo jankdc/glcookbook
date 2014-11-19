5612d9ed962680a156138908f2834f73ec1e5e8d
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2014-11-19 23:29:14 +0000

    Added main setup for OpenGL drawing and a triangle on the screen.

--- src/triangle/main.cpp
+++ src/triangle/main.cpp
@@ -1,11 +1,29 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 const auto WINDOW_WIDTH  = 800;
 const auto WINDOW_HEIGHT = 600;
-const auto WINDOW_TITLE  = "GL Cook Book - Window Creation";
+const auto WINDOW_TITLE  = "GL Cook Book - Creating a Triangle";
 
-void onKeyChange(GLFWwindow* window, int key, int scancode, int action, int mode);
+const auto VERTEX_SHADER_SOURCE = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec2 position;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(position.x, position.y, 0.0, 1.0);\n"
+    "}\n";
+
+const auto FRAGMENT_SHADER_SOURCE = 
+    "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n";
+
+void onKeyUpdate(GLFWwindow* window, int key, int code, int action, int mode);
 
 int main(int argc, char const *argv[])
 {
@@ -24,14 +42,81 @@ int main(int argc, char const *argv[])
         nullptr);
 
     glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, onKeyChange);
+    glfwSetKeyCallback(window, onKeyUpdate);
 
     // Give access to modern GL functions
     glewExperimental = GL_TRUE;
     glewInit();
 
-    // Sync the rendering window with GLFW window
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    GLfloat vertices[] = {
+       -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.0f,  0.5f
+    };
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &VERTEX_SHADER_SOURCE, nullptr);
+    glCompileShader(vertexShader);
+
+    {
+        GLint success;
+        GLchar infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+        if (! success) {
+            glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+            std::cout << "[error]: Vertex shader failed to compile.\n";
+            std::cout << infoLog << "\n";
+        }
+    }
+
+    auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &FRAGMENT_SHADER_SOURCE, nullptr);
+    glCompileShader(fragmentShader);
+
+    {
+        GLint success;
+        GLchar infoLog[512];
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+        if (! success) {
+            glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+            std::cout << "[error]: Fragment shader failed to compile.\n";
+            std::cout << infoLog << "\n";
+        }
+    }
+
+    auto shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    {
+        GLint success;
+        GLchar infoLog[512];
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+        if (! success) {
+            glGetShaderInfoLog(shaderProgram, 512, nullptr, infoLog);
+            std::cout << "[error]: Shader program failed to compile.\n";
+            std::cout << infoLog << "\n";
+        }
+    }
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    glDisableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     while (! glfwWindowShouldClose(window)) 
     {
@@ -39,15 +124,24 @@ int main(int argc, char const *argv[])
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        glDisableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
 
         glfwSwapBuffers(window);
     }
 
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
 
-void onKeyChange(GLFWwindow* window, int key, int scancode, int action, int mode)
+void onKeyUpdate(GLFWwindow* window, int key, int code, int action, int mode)
 {
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
