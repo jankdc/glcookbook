1ea7c941bda1b1f615eec11ac4db096e422881d7
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2014-11-19 09:49:19 +0000

    Added blueish background to window_creation.

--- src/window_creation/main.cpp
+++ src/window_creation/main.cpp
@@ -33,6 +33,10 @@ int main(int argc, char const *argv[])
     while (! glfwWindowShouldClose(window)) 
     {
         glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
     }
 
