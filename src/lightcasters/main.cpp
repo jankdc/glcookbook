aec4ad1b433471ebd16577481ec300f42606f23c
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2014-12-26 18:04:56 +0000

    lightcasters: Put common resources outside main.

--- src/lightcasters/main.cpp
+++ src/lightcasters/main.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <unordered_map>
 
+
 struct Light {
     glm::vec3 pos;
     glm::vec3 ka;
@@ -19,12 +20,14 @@ struct Light {
     glm::vec3 ks;
 };
 
+
 struct Material {
     glm::vec3 ka;
     glm::vec3 kd;
     glm::vec3 ks;
     float sh;
 };
+
 
 struct Cube {
     Material  material;
@@ -36,6 +39,8 @@ const auto WINDOW_WIDTH  = 800;
 const auto WINDOW_HEIGHT = 600;
 const auto WINDOW_TITLE  = "GL Cook Book - Playing with Light Casters.";
 const auto RATIO = static_cast<float>(WINDOW_WIDTH/WINDOW_HEIGHT);
+
+
 const auto VERTICES = std::vector<GLfloat> {
     // Positions          // Normals           // Texture Coords
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
@@ -78,8 +83,77 @@ const auto VERTICES = std::vector<GLfloat> {
      0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
      0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
     -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
-};
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f};
+
+
+const auto MATERIALS = std::unordered_map<std::string, Material> {
+    {"emerald", Material{
+        glm::vec3(0.02150f, 0.17450f, 0.02150f), // ka
+        glm::vec3(0.07568f, 0.61424f, 0.07568f), // kd
+        glm::vec3(0.63300f, 0.72781f, 0.63300f), // ks
+        128.0 * 0.6f // sh
+    }},
+    {"jade", Material{
+        glm::vec3(0.13500f, 0.22250f, 0.15750f), // ka
+        glm::vec3(0.54000f, 0.89000f, 0.63000f), // kd
+        glm::vec3(0.31622f, 0.31622f, 0.31622f), // ks
+        128.0f * 0.1 // sh
+    }},
+    {"obsidian", Material{
+        glm::vec3(0.05375f, 0.05000f, 0.06625f), // ka
+        glm::vec3(0.18275f, 0.17000f, 0.22525f), // kd
+        glm::vec3(0.33274f, 0.32863f, 0.34643f), // ks
+        128.0f * 0.3f // sh
+    }},
+    {"pearl", Material{
+        glm::vec3(0.25000f, 0.20725f, 0.20725f), // ka
+        glm::vec3(1.00000f, 0.82900f, 0.82900f), // kd
+        glm::vec3(0.29665f, 0.29665f, 0.29665f), // ks
+        128.0f * 0.088f // sh
+    }},
+    {"ruby", Material{
+        glm::vec3(0.17450f, 0.01175f, 0.01175f), // ka
+        glm::vec3(0.61424f, 0.04136f, 0.04136f), // kd
+        glm::vec3(0.72781f, 0.62696f, 0.62696f), // ks
+        128.0f * 0.6f // sh
+    }},
+    {"gold", Material{
+        glm::vec3(0.24725f, 0.19950f, 0.07450f), // ka
+        glm::vec3(0.75164f, 0.60648f, 0.22648f), // kd
+        glm::vec3(0.62828f, 0.55580f, 0.36606f), // ks
+        128.0f * 0.4f // sh
+    }},
+    {"cyan_plastic", Material{
+        glm::vec3(0.00000f, 0.10000f, 0.06000f), // ka
+        glm::vec3(0.00000f, 0.50981f, 0.50980f), // kd
+        glm::vec3(0.50196f, 0.50196f, 0.50196f), // ks
+        128.0f * 0.25f // sh
+    }},
+    {"bronze", Material{
+        glm::vec3(0.21250f, 0.12750f, 0.05400f), // ka
+        glm::vec3(0.71400f, 0.42840f, 0.18144f), // kd
+        glm::vec3(0.39355f, 0.27191f, 0.16672f), // ks
+        128.0f * 0.2f // sh
+    }},
+    {"bronze", Material{
+        glm::vec3(0.21250f, 0.12750f, 0.05400f), // ka
+        glm::vec3(0.71400f, 0.42840f, 0.18144f), // kd
+        glm::vec3(0.39355f, 0.27191f, 0.16672f), // ks
+        128.0f * 0.2f // sh
+    }}};
+
+
+const auto CUBES = std::vector<Cube>{
+    { MATERIALS.at("cyan_plastic"), glm::vec3( 0.0f, 0.0f, 0.0f)  },
+    { MATERIALS.at("emerald"),      glm::vec3( 2.0f, 5.0f,-15.0f) },
+    { MATERIALS.at("ruby"),         glm::vec3(-1.5f,-2.2f,-2.5f)  },
+    { MATERIALS.at("emerald"),      glm::vec3(-3.8f,-2.0f,-12.3f) },
+    { MATERIALS.at("pearl"),        glm::vec3( 2.4f,-0.4f,-3.5f)  },
+    { MATERIALS.at("pearl"),        glm::vec3(-1.7f, 3.0f,-7.5f)  },
+    { MATERIALS.at("obsidian"),     glm::vec3( 1.3f,-2.0f,-2.5f)  },
+    { MATERIALS.at("gold"),         glm::vec3( 1.5f, 2.0f,-2.5f)  },
+    { MATERIALS.at("jade"),         glm::vec3( 1.5f, 0.2f,-1.5f)  },
+    { MATERIALS.at("bronze"),       glm::vec3(-1.3f, 1.0f,-1.5f)  }};
 
 
 int main(int argc, char const *argv[])
@@ -170,98 +244,6 @@ int main(int argc, char const *argv[])
     light.kd  = glm::vec3(1.0f);
     light.ks  = glm::vec3(1.0f);
     light.pos = glm::vec3(1.2f, 1.0f, 2.0f);
-
-
-    /*
-     __  __       _______ ______ _____  _____          _
-    |  \/  |   /\|__   __|  ____|  __ \|_   _|   /\   | |
-    | \  / |  /  \  | |  | |__  | |__) | | |    /  \  | |
-    | |\/| | / /\ \ | |  |  __| |  _  /  | |   / /\ \ | |
-    | |  | |/ ____ \| |  | |____| | \ \ _| |_ / ____ \| |____
-    |_|  |_/_/    \_\_|  |______|_|  \_\_____/_/    \_\______|
-
-    */
-
-    std::unordered_map<std::string, Material> materials;
-    materials.emplace("emerald", Material{
-        glm::vec3(0.02150f, 0.17450f, 0.02150f), // ka
-        glm::vec3(0.07568f, 0.61424f, 0.07568f), // kd
-        glm::vec3(0.63300f, 0.72781f, 0.63300f), // ks
-        128.0 * 0.6f // sh
-    });
-    materials.emplace("jade", Material{
-        glm::vec3(0.13500f, 0.22250f, 0.15750f), // ka
-        glm::vec3(0.54000f, 0.89000f, 0.63000f), // kd
-        glm::vec3(0.31622f, 0.31622f, 0.31622f), // ks
-        128.0f * 0.1 // sh
-    });
-    materials.emplace("obsidian", Material{
-        glm::vec3(0.05375f, 0.05000f, 0.06625f), // ka
-        glm::vec3(0.18275f, 0.17000f, 0.22525f), // kd
-        glm::vec3(0.33274f, 0.32863f, 0.34643f), // ks
-        128.0f * 0.3f // sh
-    });
-    materials.emplace("pearl", Material{
-        glm::vec3(0.25000f, 0.20725f, 0.20725f), // ka
-        glm::vec3(1.00000f, 0.82900f, 0.82900f), // kd
-        glm::vec3(0.29665f, 0.29665f, 0.29665f), // ks
-        128.0f * 0.088f // sh
-    });
-    materials.emplace("ruby", Material{
-        glm::vec3(0.17450f, 0.01175f, 0.01175f), // ka
-        glm::vec3(0.61424f, 0.04136f, 0.04136f), // kd
-        glm::vec3(0.72781f, 0.62696f, 0.62696f), // ks
-        128.0f * 0.6f // sh
-    });
-    materials.emplace("gold", Material{
-        glm::vec3(0.24725f, 0.19950f, 0.07450f), // ka
-        glm::vec3(0.75164f, 0.60648f, 0.22648f), // kd
-        glm::vec3(0.62828f, 0.55580f, 0.36606f), // ks
-        128.0f * 0.4f // sh
-    });
-    materials.emplace("cyan_plastic", Material{
-        glm::vec3(0.00000f, 0.10000f, 0.06000f), // ka
-        glm::vec3(0.00000f, 0.50981f, 0.50980f), // kd
-        glm::vec3(0.50196f, 0.50196f, 0.50196f), // ks
-        128.0f * 0.25f // sh
-    });
-    materials.emplace("bronze", Material{
-        glm::vec3(0.21250f, 0.12750f, 0.05400f), // ka
-        glm::vec3(0.71400f, 0.42840f, 0.18144f), // kd
-        glm::vec3(0.39355f, 0.27191f, 0.16672f), // ks
-        128.0f * 0.2f // sh
-    });
-    materials.emplace("bronze", Material{
-        glm::vec3(0.21250f, 0.12750f, 0.05400f), // ka
-        glm::vec3(0.71400f, 0.42840f, 0.18144f), // kd
-        glm::vec3(0.39355f, 0.27191f, 0.16672f), // ks
-        128.0f * 0.2f // sh
-    });
-
-
-    /*
-      _____ _    _ ____  ______  _____
-     / ____| |  | |  _ \|  ____|/ ____|
-    | |    | |  | | |_) | |__  | (___
-    | |    | |  | |  _ <|  __|  \___ \
-    | |____| |__| | |_) | |____ ____) |
-     \_____|\____/|____/|______|_____/
-
-    */
-
-    auto cubes = std::vector<Cube>{
-        { materials.at("cyan_plastic"), glm::vec3( 0.0f, 0.0f, 0.0f)  },
-        { materials.at("emerald"),      glm::vec3( 2.0f, 5.0f,-15.0f) },
-        { materials.at("ruby"),         glm::vec3(-1.5f,-2.2f,-2.5f)  },
-        { materials.at("emerald"),      glm::vec3(-3.8f,-2.0f,-12.3f) },
-        { materials.at("pearl"),        glm::vec3( 2.4f,-0.4f,-3.5f)  },
-        { materials.at("pearl"),        glm::vec3(-1.7f, 3.0f,-7.5f)  },
-        { materials.at("obsidian"),     glm::vec3( 1.3f,-2.0f,-2.5f)  },
-        { materials.at("gold"),         glm::vec3( 1.5f, 2.0f,-2.5f)  },
-        { materials.at("jade"),         glm::vec3( 1.5f, 0.2f,-1.5f)  },
-        { materials.at("bronze"),       glm::vec3(-1.3f, 1.0f,-1.5f)  }
-    };
-
 
     /*
      __  __          _____ _   _   _      ____   ____  _____
@@ -374,7 +356,7 @@ int main(int argc, char const *argv[])
             glUniform1f(matShineId, 64.0f);
 
             glBindVertexArray(cubeMeshId);
-            for (auto cube : cubes) {
+            for (auto cube : CUBES) {
                 auto position = cube.position;
                 auto model = glm::mat4(1.0f);
                 auto modelRotationAngle = glm::radians(-55.0f);
